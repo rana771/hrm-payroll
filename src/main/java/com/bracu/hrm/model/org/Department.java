f116548e592ff8b58fd8e36e8d8b0a1343ce8d55
Rana <rana@USERs-MacBook-Pro.local>
2018-01-25 18:01:36 +0600

    composite key added in department

--- src/main/java/com/bracu/hrm/model/org/Department.java
+++ src/main/java/com/bracu/hrm/model/org/Department.java
@@ -13,9 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "department")
+@Table(name = "department",uniqueConstraints = { @UniqueConstraint( columnNames = {"company_id", "code"}),
+		@UniqueConstraint( columnNames = {"company_id", "name"}),@UniqueConstraint( columnNames = {"company_id", "short_name"}) })
 @Data
 public class Department extends BaseEntity {
 
@@ -23,13 +25,13 @@ public class Department extends BaseEntity {
 	@JoinColumn(name = "company_id" )
 	private Company company;
 	
-	@Column(nullable =  false, unique = true)
+	@Column(nullable =  false)
 	private String code;
 	
-	@Column(nullable =  false, unique = true)
+	@Column(nullable =  false)
 	private String name;
 	
-	@Column(name = "short_name", unique  = true)
+	@Column(name = "short_name")
 	private String shortName;
 	
 	
