b47790add62466141a8d3f5ea38b83a52dc080fa
Biprojit <bipro77@gmail.com>
2018-01-25 15:41:40 +0600

    Domain Change

--- src/main/java/com/bracu/hrm/model/settings/JobLevel.java
+++ src/main/java/com/bracu/hrm/model/settings/JobLevel.java
@@ -1,15 +1,19 @@
 package com.bracu.hrm.model.settings;
 
 import com.bracu.hrm.model.BaseEntity;
+import com.bracu.hrm.model.org.Company;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Entity
 @Table(name = "job_level")
 @Data
 public class JobLevel extends BaseEntity{
+
+    @ManyToOne(targetEntity = Company.class, fetch = FetchType.LAZY,cascade = CascadeType.DETACH)
+    @JoinColumn(name = "company_id",nullable = false)
+    private Company company;
 
     private String level;
 
