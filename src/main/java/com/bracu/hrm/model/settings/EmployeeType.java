7a3acb8fcdcbde5efa43dfad220b505a1d2e9867
Biprojit <bipro77@gmail.com>
2018-01-25 15:42:01 +0600

    Domain Change

--- src/main/java/com/bracu/hrm/model/settings/EmployeeType.java
+++ src/main/java/com/bracu/hrm/model/settings/EmployeeType.java
@@ -1,11 +1,10 @@
 package com.bracu.hrm.model.settings;
 
 import com.bracu.hrm.model.BaseEntity;
+import com.bracu.hrm.model.org.Company;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 //Employee type like Permanent, contractual etc
 
@@ -14,6 +13,9 @@ import javax.persistence.Table;
 @Data
 public class EmployeeType extends BaseEntity{
 
+	@ManyToOne(targetEntity = Company.class, fetch = FetchType.LAZY,cascade = CascadeType.DETACH)
+	@JoinColumn(name = "company_id",nullable = false)
+	private Company company;
 	@Column(unique=true)
 	private String name;
 
